756541d28f153cf2c77ec5dd8291f8d1f1e5049e
Rezwan Mahmud <mahmuydrayhan029@gmail.com>
2023-03-03 04:52:51 +0600

    sort and filter feature added

--- js/main.js
+++ js/main.js
@@ -1,19 +1,34 @@
+const demoImage = 'https://images.unsplash.com/photo-1618827840222-fcf8f42509c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1031&q=80'
+
 const getData = async (limit) => {
   try {
     const resp = await fetch(`https://forbes400.onrender.com/api/forbes400?limit=${limit}`);
     const data = await resp.json();
-    showData(limit, data);
+    showData(limit, data, false);
   }
   catch (error) {
     console.log(error);
   }
 }
 
-const showData = (limit, data) => {
+const getFilterData = async (filterValue) => {
+  try {
+    const resp = await fetch(`https://forbes400.onrender.com/api/forbes400/${filterValue}`);
+    let data = await resp.json();
+    // data = data.slice(0, 50)
+    showData(50, data, true, filterValue);
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+const showData = (limit, data, filter, filterValue) => {
   const tableBody = document.getElementById('table-body');
+  tableBody.innerHTML = '';
   let i = 2;
   data.forEach(data => {
-    const { person, countryOfCitizenship, industries, finalWorth, rank } = data;
+    const { person, countryOfCitizenship, industries, finalWorth, rank, imageExists } = data;
     const personName = person.name;
     const image = person.squareImage;
     const country = countryOfCitizenship;
@@ -28,7 +43,7 @@ const showData = (limit, data) => {
         <div class="flex items-center space-x-3">
           <div class="avatar">
             <div class="mask mask-squircle w-12 h-12">
-              <img src="${image}" alt="Avatar Tailwind CSS Component" />
+              <img src="${imageExists ? image : demoImage}" alt="Avatar Tailwind CSS Component" />
             </div>
           </div>
           <div>
@@ -43,7 +58,8 @@ const showData = (limit, data) => {
       <td>${realTimeRank}</td>
       <td>
         <h1 class="text-xl mb-2">$${worth}K</h1>
-        <label class="btn btn-accent btn-sm w-full" for="my-modal-3" onclick="fetchIndividualData(${limit}, ${realTimeRank})">details</label>
+        <label class="btn btn-accent btn-sm w-full" for="my-modal-3" onclick="fetchIndividualData(${limit}, ${realTimeRank}, ${filter}, '${filterValue}')">details</label>
+
       </td>
         `
     tableBody.appendChild(tableRow);
@@ -52,18 +68,31 @@ const showData = (limit, data) => {
   })
 }
 
-const fetchIndividualData = async (limit, id) => {
-  const modalElement = document.getElementById('modal-body');
-  modalElement.innerHTML = ''
-  const resp = await fetch(`https://forbes400.onrender.com/api/forbes400?limit=${limit}`);
-  const data = await resp.json();
-  getIndividualData(id, data);
+const fetchIndividualData = async (limit, id, filter, filterValue) => {
+  if (filter === false) {
+    const modalElement = document.getElementById('modal-body');
+    modalElement.innerHTML = '';
+    const resp = await fetch(`https://forbes400.onrender.com/api/forbes400?limit=${limit}`);
+    const data = await resp.json();
+    getIndividualData(id, data);
+  }
+  else {
+    console.log(filterValue);
+    const modalElement = document.getElementById('modal-body');
+    modalElement.innerHTML = ''
+    const resp = await fetch(`https://forbes400.onrender.com/api/forbes400/${filterValue}`);
+  
+    let data = await resp.json();
+    console.log(data);
+    getIndividualData(id, data);
 
+  }
 }
 
 const getIndividualData = (id, data) => {
   const personData = data.find(person => person.rank == id)
-  const { person, countryOfCitizenship, industries, finalWorth, rank, bios, source, city, state , birthDate, gender, financialAssets} = personData;
+  console.log(personData);
+  const { person, countryOfCitizenship, industries, finalWorth, rank, bios, source, city, state, birthDate, gender, financialAssets, imageExists } = personData;
   const modalElement = document.getElementById('modal-body');
   modalElement.innerHTML = `
     <div class="modal-box relative w-3/4 max-w-none">
@@ -74,7 +103,7 @@ const getIndividualData = (id, data) => {
     
     <div class="flex gap-4 my-4">
   <div>
-    <img src="${person.squareImage}" alt="" class="mb-2">
+    <img src="${imageExists ? person.squareImage : demoImage}" alt="" class="mb-2">
     <h3 class="text-lg font-semibold">Sources:<span class="font-normal"> ${source}</span></h3>
   </div>
   <div class="leading-8 flex flex-col justify-between">
@@ -83,13 +112,13 @@ const getIndividualData = (id, data) => {
     <h2 class="text-base font-semibold">State:<span class="font-normal"> ${state}</span></h2>
     <h2 class="text-base font-semibold">City:<span class="font-normal"> ${city}</span></h2>
     <h2 class="text-base font-semibold">Birthday:<span class="font-normal"> ${toDateFormat(birthDate)}</span></h2>
-    <h2 class="text-base font-semibold">Gender:<span class="font-normal"> ${gender === 'M'? "Male" : "Female"} </span></h2>
+    <h2 class="text-base font-semibold">Gender:<span class="font-normal"> ${gender === 'M' ? "Male" : "Female"} </span></h2>
 
     <h3 class="text-2xl font-semibold underline">Financial Information</h3>
-    <h2 class="text-base font-semibold">Exchange:<span class="font-normal"> ${financialAssets.map(item => item.exchange).join(', ')}</span></h2>
-    <h2 class="text-base font-semibold">Ticker:<span class="font-normal"> ${financialAssets.map(item => item.ticker).join(', ')}</span></h2>
-    <h2 class="text-base font-semibold">Total Shares:<span class="font-normal"> ${financialAssets.map(item => item.numberOfShares).join(', ')}</span></h2>
-    <h2 class="text-base font-semibold">Share Price:<span class="font-normal"> ${financialAssets.map(item => item.sharePrice).join(', ')}</span></h2>
+    <h2 class="text-base font-semibold">Exchange:<span class="font-normal"> ${Array.isArray(financialAssets) ? financialAssets.map(item => item.exchange).join(', '):'N/A'}</span></h2>
+    <h2 class="text-base font-semibold">Ticker:<span class="font-normal"> ${Array.isArray(financialAssets)?financialAssets.map(item => item.ticker).join(', '): 'N/A'}</span></h2>
+    <h2 class="text-base font-semibold">Total Shares:<span class="font-normal"> ${Array.isArray(financialAssets)?financialAssets.map(item => item.numberOfShares).join(', '): 'N/A'}</span></h2>
+    <h2 class="text-base font-semibold">Share Price:<span class="font-normal"> ${Array.isArray(financialAssets)?financialAssets.map(item => item.sharePrice).join(', '): 'N/A'}</span></h2>
     <h2 class="text-base font-semibold">:<span class="font-normal"> </span></h2>
   </div>
 </div>
@@ -100,4 +129,24 @@ const getIndividualData = (id, data) => {
 const toDateFormat = date => {
   const dateFormated = new Date(date);
   return dateFormated.toUTCString();
+}
+
+const sortDataLimit = () => {
+  const sortText = document.getElementById('sort-option').value;
+  const sortValue = sortText.slice(4) != '' ? sortText.slice(4) : 'getAllBillionaires';
+  const sortElement = document.getElementById('sort-limit');
+  if (sortValue === 'getAllBillionaires') {
+    getData(400)
+    sortElement.innerText = '400';
+  }
+  else {
+    getData(sortValue);
+    sortElement.innerText = sortValue;
+  }
+}
+const filterData = () => {
+  const filterValue = document.getElementById('filter-option').value;
+  const filterElement = document.getElementById('filter-element');
+  getFilterData(filterValue);
+  filterElement.innerText = `The list of Forbes ${filterValue} Billionaires`;
 }
